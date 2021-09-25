e40fc3238e018bb7e410eb0bed79be8a93830ff0
bakhtiarabid <abid.bu16@gmail.com>
2021-09-26 02:35:29 +0600

    Fake Data javascript Object Created

---
+++ README.md
@@ -0,0 +1,5 @@
+# Top Leaders Team
+
+### [Assignment private repo link](https://github.com/ProgrammingHeroWC4/the-superhero-direction-bakhtiarabid)
+
+#### Live link:
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+   return (
+      <div className="App">
+         <h1>Hello</h1>
+         <img
+            src="https://i.ibb.co/yqJzX13/Getty-Images-1036094130-cf8da629f9b44c81a0b1398311ac5787.webp"
+            alt="Getty-Images-1036094130-cf8da629f9b44c81a0b1398311ac5787"
+            border="0"
+         />
+      </div>
+   );
+}
+
+export default App;
